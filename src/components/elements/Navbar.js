b1572f689b222a152376c78a9c89a637e82d9742
Jonathan Ro <jonro.2955@gmail.com>
2022-04-01 15:21:09 -0700

    Set up component structure, pages, and page routing

---
+++ src/components/elements/Navbar.js
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+    <nav className='nav-wrapper red darken-3'>
+      <div className='container'>
+        <a href='/'>Logo</a>
+        <ul className='right'>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/shop'>Shop</Link>
+          </li>
+          <li>
+            <Link to='/cart'>Cart</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
