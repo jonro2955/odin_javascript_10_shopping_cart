81cc725a4d3a1fd3eaedb99c7d15bef3261790ee
Jonathan Ro <jonro.2955@gmail.com>
2022-04-01 16:27:40 -0700

    Set up homepage text and background image

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,11 @@
 // import React from 'react';
 
 export default function Home() {
-  return <h1> Home </h1>;
+  return (
+    <div id='homepage'>
+      <h1> Home </h1>
+      <h2>Welcome to Spectrum.</h2>
+      <div>We the hue you're looking for color.</div>
+    </div>
+  );
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOMClient from 'react-dom/client';
+
 import './index.css';
 //Must run 'npm i react-router-dom' before using router:
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -9,8 +10,9 @@ import Shop from './components/pages/Shop';
 import Cart from './components/pages/Cart';
 import Navbar from './components/elements/Navbar';
 
-
-ReactDOM.render(
+// Create a root as per React 18 (https://github.com/reactwg/react-18/discussions/5)
+const root = ReactDOMClient.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Navbar />
@@ -20,6 +22,5 @@ ReactDOM.render(
         <Route path='/cart' element={<Cart />} />
       </Routes>
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
